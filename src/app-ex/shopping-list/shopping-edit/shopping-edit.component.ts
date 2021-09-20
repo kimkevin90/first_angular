14338b886f1b2ca106feb70487d9594dbbf35927
kimkevin90 <65535673+kimkevin90@users.noreply.github.com>
2021-09-20 11:39:02 +0900

    0920

--- src/app-ex/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app-ex/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -6,7 +6,7 @@ import {
   Output,
   ViewChild,
 } from '@angular/core';
-import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Ingredient } from 'src/app-ex/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
